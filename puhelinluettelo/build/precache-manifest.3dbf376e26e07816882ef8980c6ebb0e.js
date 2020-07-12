1b1ab2648736d490671d3ded935c9f61f3db6ae9
mmyotane <miika.myotanen@hotmail.com>
2020-07-12 14:37:04 +0300

    heroku

---
+++ puhelinluettelo/build/precache-manifest.3dbf376e26e07816882ef8980c6ebb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045735113b47ad5fd91cff36ad7f3b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "710a43d008598fb0bb5c",
+    "url": "/static/css/main.af05c9d8.chunk.css"
+  },
+  {
+    "revision": "b83d5d237c05fa753d51",
+    "url": "/static/js/2.4d51bd9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d51bd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710a43d008598fb0bb5c",
+    "url": "/static/js/main.2a363027.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
